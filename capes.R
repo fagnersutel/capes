293c2e978ec4b3705e9618d632de4c9498e73b65
fagnersutel <fagnersutel@hotmail.com>
2018-03-27 09:54:33 -0300

    Add files via upload

--- capes.R
+++ capes.R
@@ -1,5 +1,5 @@
 #https://stackoverflow.com/questions/1299871/how-to-join-merge-data-frames-inner-outer-left-right
-setwd("~/OneDrive/Cursos/meusR/capes/")
+setwd("capes/")
 scimago <- read.csv("scimagojr 2016.csv", header = T, sep = ";")
 names(scimago)
 head(scimago)
@@ -13,7 +13,9 @@ scimago$Type <- NULL
 scimago$SJR.Best.Quartile <- NULL
 scimago$SJR <- NULL
 head(scimago)
-
+dim(scimago)
+scimago <- scimago[!duplicated(scimago$Issn), ]
+dim(scimago)
 
 df1 = read.table("classificacoes_publicadas_medicina_i_2017_1496941695476.xls", header = TRUE)
 head(df1)
@@ -36,23 +38,19 @@ dim(df6)
 df7 = read.table("classificacoes_publicadas_farmacia_2017_1496941694770.xls", header = TRUE)
 head(df7)
 dim(df7)
-
-
-
 dffinal <- rbind(df1, df2, df3, df4, df5, df6, df7)
 dim(dffinal)
-
-teste <- duplicated(dffinal)
-which(duplicated(dffinal))
-
 qualis <- dffinal[!duplicated(dffinal), ]
 dim(qualis)
+qualis <- qualis[!duplicated(qualis$ISSN), ]
+dim(qualis)
 head(qualis)
-write.xlsx(qualis, "qualis.xlsx")
+#write.xlsx(qualis, "qualis.xlsx")
 
-
+#install.packages('xlsx')
 require(xlsx) 
-egressos <- read.xlsx("lattes.xls", 1)
+options(java.parameters = "- Xmx8000m")
+egressos <- read.xlsx("lattes.xls", 1, encoding="UTF-8")
 egressos$DOI <- NULL
 egressos$VOLUME <- NULL
 egressos$PAG_INICIAL <- NULL
@@ -64,34 +62,52 @@ head(scimago)
 head(qualis)
 
 
-tabelaum <- merge(egressos, qualis, by.x = "ISSN", by.y = "ISSN")
-View(tabelaum)
+#tabelaum <- merge(egressos, qualis, by.x = "ISSN", by.y = "ISSN")
+#dim(tabelaum)
+#View(tabelaum)
+#write.xlsx(tabelaum, "qualis_e_egressos.xlsx")
 
-tabeladois <- merge(tabelaum, scimago, by.x = "ISSN", by.y = "Issn")
-View(tabeladois)
-dim(tabeladois)
+#tabeladois <- merge(tabelaum, scimago, by.x = "ISSN", by.y = "Issn")
+#View(tabeladois)
+#dim(tabeladois)
 
 
-final <- tabeladois[!duplicated(tabeladois), ]
-dim(final)
+#final <- tabeladois[!duplicated(tabeladois), ]
+#dim(final)
 
-write.xlsx(final, "qualis_egressos.xlsx")
+#write.xlsx(final, "qualis_egressos.xlsx")
 
 
 #install.packages('sqldf')
 names(egressos) <- c("Paper", "Journal", "ISSN", "AUTOR")
 library(sqldf)
-teste1 <- sqldf("SELECT AUTOR,  ISSN, Estrato, Paper
-             FROM egressos
-             INNER  JOIN qualis USING(ISSN)")
-dim(teste1)
-View(df4)
+#teste1 <- sqldf("SELECT AUTOR,  ISSN, Estrato, Paper
+#             FROM egressos
+#             INNER  JOIN qualis USING(ISSN)")
+#dim(egressos)
+#dim(teste1)
+#View(teste1)
 
-teste2 <- sqldf("SELECT DISTINCT *
+teste2 <- sqldf("SELECT  *
                 FROM egressos 
                 LEFT JOIN qualis ON egressos.ISSN = qualis.ISSN")
+dim(egressos)
 dim(teste2)
+names(teste2) <- c("Paper", "Journal", "ISSN", "AUTOR", "conmtrole", "Titulo", "Estrato")
 
+#scimago <- scimago[complete.cases(scimago$Issn), ]
+#teste2 <- teste2[complete.cases(teste2$conmtrole), ]
+
+teste3 <- sqldf("SELECT  *
+                FROM teste2 
+                LEFT JOIN scimago ON teste2.ISSN = scimago.Issn")
+dim(teste3)
+teste3 <- cbind(teste3$AUTOR, teste3)
+teste3[,5] <- NULL
+teste3$Issn <- NULL
+teste3$conmtrole <- NULL
+names(teste3) <- c("AUTOR", "Paper", "Journal", "ISSN", "Titulo", "Estrato", "Title", "H.index")
+write.xlsx(teste3, "qualis_egressos_JCR.xlsx")
 
 
 authors <- data.frame(
@@ -112,13 +128,6 @@ books <- data.frame(
     other.author = c(NA, "Ripley", NA, NA, NA, NA,
                      "Venables & Smith"))
 
-(m0 <- merge(authorN, books))
-(m1 <- merge(authors, books, by.x = "surname", by.y = "name"))
+(m0 <- merge(egressos, qualis))
+(m1 <- merge(egressos, qualis, by.x = "ISSN", by.y = "ISSN"))
 m2 <- merge(books, authors, by.x = "name", by.y = "surname")
-stopifnot(exprs = {
-    identical(m0, m2[, names(m0)])
-    as.character(m1[, 1]) == as.character(m2[, 1])
-    all.equal(m1[, -1], m2[, -1][ names(m1)[-1] ])
-    identical(dim(merge(m1, m2, by = NULL)),
-              c(nrow(m1)*nrow(m2), ncol(m1)+ncol(m2)))
-})
